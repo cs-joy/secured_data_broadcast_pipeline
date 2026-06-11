8f7edcfee041cb9906bd5ee0cbe21f07b205ab70
cs-joy <mdzahangiralam1001@gmail.com>
2026-06-11 18:52:47 +0600

    deployment script..

--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -7,8 +7,23 @@ async function main() {
 
     // get network information
     const network = hre.network.name;
-    console.log(`🌚 Deploying to network: ${network}`);
+    console.log(`❄️ Deploying to network: ${network}`);
 
     // get deployer account
+    const [deployer] = await hre.ethers.getSigners();
+    console.log(`🦹‍♂️ Deployer address: ${deployer.address}`);
+    console.log(`🍥 Deployer balance: ${(await deployer.getBalance()).toString()} wei`);
+
+
+    // deploy DataEncryption contract
+    console.log(`\n📦 Deploying DataEncryption...`);
+    const DataEncryption = await hre.ethers.getContractFactory("DataEncryption");
+    const dataEncryption = await DataEncryption.deploy();
+    await dataEncryption.deployed();
+    console.log(`✅ DataEncryption deployed to: ${dataEncryption.address}`);
+
+
+    // deploy FederatedLearning contract
+    console.log("\n📦 Deploying FederatedLearning...");
 
 }
